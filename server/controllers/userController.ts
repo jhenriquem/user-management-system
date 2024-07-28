bbf0f2f1a5ef7bc1be4aaadc6162da71ccbd7182
jhenriquem <machadojh11@gmail.com>
2024-07-27 22:36:04 -0300

    changing URIs

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import findUser from "../services/findUser";
 
-export default async function profileController(req: Request, res: Response) {
+export default async function userController(req: Request, res: Response) {
   try {
     const id = req.body.userId
     const response = await findUser(id)
@@ -10,7 +10,7 @@ export default async function profileController(req: Request, res: Response) {
       return res.status(500).json({ statusMessage: "Error when trying to find the user", error: response.error })
     }
 
-    return res.status(200).json({ message: response })
+    return res.status(200).json({ statusMessage: response.status, data: response.data })
   } catch (err: any) {
     return res.status(500).json({ statusMessage: "Error when trying to find the user", error: err.message })
   }
